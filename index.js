e0266bd28203b441130b2748830decb53585b3bf
Sanjucta Ghose <sanjuctab@gmail.com>
2018-03-08 15:51:39 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+var request = require("request");
+
+var Upbit = function()
+{
+	this.url = "https://api.lbank.info/v1/";
+}
+
+Upbit.prototype.getTrades =  function (market, cb, count) {
+	var path, url, qs;
+
+	path = "trades.do";
+	qs = "?symbol=";
+
+	url = this.url+path+qs+market;
+
+	if(count)
+	{
+		url += "&size="+count;
+	}
+
+	return request(url, (err, response, body) => {
+		let error, result
+		if (err || (response.statusCode !== 200 && response.statusCode !== 400)) {
+			return cb(new Error(err != null ? err : response.statusCode))
+		}
+		try {
+			result = JSON.parse(body)
+		} catch (error1) {
+			error = error1
+			return cb(null, {
+				message: body.toString()
+			})
+		}
+		if (result.message != null) {
+			return cb(new Error(result.message))
+		}
+		return cb(null, result)
+	})
+}
+
+
+module.exports = Upbit;
